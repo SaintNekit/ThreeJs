0d1566b8da80dc751a2c3d4c01a48832ce6e4de1
SaintNekit <gulivon@mail.ru>
2020-06-18 18:18:11 +0300

    add: dat gui options for planets

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,31 +1,36 @@
 import React from 'react';
 import * as THREE from 'three';
+import * as dat from 'dat.gui';
 import { removeLights, createEarth, createClouds, createStars, createSun, createMoon } from '../utils/helpers';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { TweenMax, TimelineMax } from 'gsap';
+
 
 const SolarSystem = () => {
   let display = true;
   let reverse = false;
 
+  const gui = new dat.GUI();
   const scene = new THREE.Scene();
   const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.01, 10000 );
   const renderer = new THREE.WebGLRenderer( { antialias: true });
   const light = new THREE.PointLight(0xffffff, 1);
   const ambLight = new THREE.AmbientLight(0x333333);
   const earthOrbit = new THREE.Object3D();
-  const zeroValue = 0;
-  let earthAngle = 0;
-  let moonAngle = 0;
+  const zeroValue = 0.01;
+  let earthAngle = zeroValue;
+  let moonAngle = zeroValue;
   const raycaster = new THREE.Raycaster();
   const mouse = new THREE.Vector2();
   const earthVector = new THREE.Vector3(0.5, 1, zeroValue);
   const cloudVector = new THREE.Vector3(0.5, 1, -0.5);
   const sunVector = new THREE.Vector3(zeroValue, 0.3, zeroValue);
-  const rotationSpeed = 0.01;
   const starsTimeLine = new TimelineMax({ yoyo: true });
   const planetsTimeLine = new TimelineMax();
-  
+  let sunSpinningSpeed = zeroValue;
+  let earthSpinningSpeed = zeroValue;
+  let earthFlyingSpeed = zeroValue;
+  let moonFlyingSpeed = zeroValue;
   
   const sun = createSun();
   const earth = createEarth();
@@ -39,6 +44,28 @@ const SolarSystem = () => {
   }
 
   const init = () => {
+    gui.width = '300px';
+    const folder = gui.addFolder('Options');
+    const settings = {
+      'sun spinning speed': sunSpinningSpeed,
+      'earth spinning speed': earthSpinningSpeed,
+      'earth rotation speed': earthFlyingSpeed,
+      'moon rotation speed': moonFlyingSpeed,
+    };
+    folder.add(settings, 'sun spinning speed', -1.0, 1.0).onChange((speed) => {
+      sunSpinningSpeed = speed;
+    });
+    folder.add(settings, 'earth spinning speed', -1.0, 1.0).onChange((speed) => {
+      earthSpinningSpeed = speed;
+    });
+    folder.add(settings, 'earth rotation speed', -1.0, 1.0).onChange((speed) => {
+      earthFlyingSpeed = speed;
+    });
+    folder.add(settings, 'moon rotation speed', -1.0, 1.0).onChange((speed) => {
+      moonFlyingSpeed = speed;
+    })
+    folder.open();
+
     camera.position.set(zeroValue, 2, 7);
     light.intensity = zeroValue;
     sun.scale.set(zeroValue);
@@ -102,12 +129,12 @@ const SolarSystem = () => {
   };
 
   const animate = () => {
-    earth.rotateOnAxis(earthVector.normalize(), rotationSpeed);
-    clouds.rotateOnAxis(cloudVector.normalize(), rotationSpeed * 2);
-    sun.rotateOnAxis(sunVector.normalize(), rotationSpeed);
+    earth.rotateOnAxis(earthVector.normalize(), earthSpinningSpeed);
+    clouds.rotateOnAxis(cloudVector.normalize(), earthSpinningSpeed * 2);
+    sun.rotateOnAxis(sunVector.normalize(), sunSpinningSpeed);
     
-    earthAngle += rotationSpeed * (Math.PI / 3);
-    moonAngle += rotationSpeed * (Math.PI / 0.8);
+    earthAngle += earthFlyingSpeed * (Math.PI / 3);
+    moonAngle += moonFlyingSpeed * (Math.PI / 0.8);
 
     earthOrbit.position.set(Math.sin(earthAngle) * 3, 0, Math.cos(earthAngle) * 2);
     moon.position.set(Math.sin(moonAngle) * 0.3, 0, Math.cos(moonAngle) * 0.3);
@@ -125,7 +152,7 @@ const SolarSystem = () => {
     const sunObj = raycaster.intersectObject(sun);
     const starsObj = raycaster.intersectObject(stars);
     
-    if(starsObj.length) {
+    if(starsObj.length && e.ctrlKey) {
       reverse = !reverse;
       reverse ? starsTimeLine.play() : starsTimeLine.reverse();
     };
